01dea1f92598c2aa2fe04c9910d18c17f9180b7f
Fauzan Muhammad <85777524+fauzan-2405@users.noreply.github.com>
2024-12-12 16:29:14 +0700

    Update so it can fill matrix B

--- bmm/matrix_fill.v
+++ bmm/matrix_fill.v
@@ -4,19 +4,32 @@
 // Engineer	: Fauzan
 ///////////////////////////////
 
-module matrix_fill(
+module matrix_fill #(
+	parameter BLOCK_SIZE = 2,
+	parameter SIZE		 = 4,
+	parameter DATA_WIDTH = 4
+)
+(
     input [31:0] A_rows,
-    output reg [3:0] A [0:1][0:3] // 2 rows and 4 columns (2x4 matrix)
+	input [31:0] B_cols,
+    output reg [3:0] B [0:SIZE-1][0:BLOCK_SIZE-1], // 4 rows and 2 columns (4x2 matrix)
+	output reg [3:0] A [0:BLOCK_SIZE-1][0:SIZE-1]//  2 rows and 4 columns (2x4 matrix)
 );
+	/*
+	parameter BLOCK_SIZE = 2;
+	parameter SIZE		 = 4;
+	parameter DATA_WIDTH = 4; */
 
     integer i, j;
 
     always @(*) begin
-        // Fill the output matrix A based on the input A_rows
-        for (i = 0; i < 2; i = i + 1) begin
-            for (j = 0; j < 4; j = j + 1) begin
+        // Fill the output matrix A & B based on the input A_rows and B_cols
+        for (i = 0; i < BLOCK_SIZE; i = i + 1) begin
+            for (j = 0; j < SIZE; j = j + 1) begin
                 // Extract 4 bits from A_rows and assign to A
-                A[i][j] = A_rows[(i * 4 + j) * 4 +: 4]; // Extract 4 bits
+                A[i][j] = A_rows[(i * SIZE + j) * DATA_WIDTH +: DATA_WIDTH]; // Extract 4 bits
+                // Extract 4 bits from B_cols and assign to B
+                B[j][i] = B_cols[(j * BLOCK_SIZE + i) * DATA_WIDTH +: DATA_WIDTH]; // Extract 4 bits
             end
         end
     end
