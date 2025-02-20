182cf2f8e315a321569c7db02f9ba1743826588a
fauzan-2405 <fauzaanmuhammaad@gmail.com>
2025-02-20 16:08:56 +0700

    update mac

--- matrix_mult/mac.v
+++ matrix_mult/mac.v
@@ -11,22 +11,22 @@ module mac #(
     parameter CHUNK_SIZE = 4,
     parameter INNER_DIMENSION = 64 // The same number of rows in one matrix and same number of columns in the other matrix
 ) (
-    input clk, rst_n,
+    input clk, rst_n, reset_acc,
     input [WIDTH-1:0] in_north0, in_north1,
     input [WIDTH-1:0] in_west0, in_west2,
-    output reg accumulator_done,
-    output reg [WIDTH*CHUNK_SIZE-1:0] out
+    output wire accumulator_done,
+    output wire [WIDTH*CHUNK_SIZE-1:0] out
 );
     // Systolic
     wire done_systolic; // output from systolic
     wire [WIDTH*CHUNK_SIZE-1:0] out_systolic;
 
-    systolic_array_2x2 #(.WIDTH(WIDTH), .FRAC_WIDTH(FRAC_WIDTH), .CHUNK_SIZE(CHUNK_SIZE)) systolic (
+    systolic_array_2x2 #(.WIDTH(WIDTH), .FRAC_WIDTH(FRAC_WIDTH)) systolic (
         .clk(clk), .rst_n(rst_n), .in_north0(in_north0), .in_north1(in_north1), .in_west0(in_west0), .in_west2(in_west2), .done(done_systolic), .out(out_systolic)
     );
 
     accumulator #(.WIDTH(WIDTH), .FRAC_WIDTH(FRAC_WIDTH), .BLOCK_SIZE(BLOCK_SIZE), .CHUNK_SIZE(CHUNK_SIZE), .INNER_DIMENSION(INNER_DIMENSION)) acc (
-        .clk(clk), .rst_n(rst_n), .in(out_systolic), .systolic_done(done_systolic), .accumulator_done(accumulator_done), .out(out)
-    )
+        .clk(clk), .rst_n(reset_acc), .in(out_systolic), .systolic_done(done_systolic), .accumulator_done(accumulator_done), .out(out)
+    );
 
 endmodule
