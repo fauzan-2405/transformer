0f4ae542c9a444d6ba14a6ada8182ac842e43e8f
fauzan-2405 <fauzaanmuhammaad@gmail.com>
2025-02-20 12:24:32 +0700

    fixed some bugs

--- matrix_mult/accumulator.v
+++ matrix_mult/accumulator.v
@@ -30,7 +30,7 @@ module accumulator #(
             out <= {WIDTH*CHUNK_SIZE{1'b0}}; // Reset all to 0
         end
         else begin
-            if (counter == INNER_DIMENSION / BLOCK_SIZE) begin
+            if (counter == (INNER_DIMENSION / BLOCK_SIZE)) begin
                 accumulator_done <= 1;
 				// Concatenate all update_value registers for the output
                 out[(WIDTH*1)-1:WIDTH*0] <= update_value[3];
@@ -44,7 +44,7 @@ module accumulator #(
                 counter <= counter + 1;
             end
 		// Accumulate the input values
-		update_value[0] <= update_value[0] + in[(WIDTH*CHUNK_SIZE-1)-WIDTH*0:(WIDTH*CHUNK_SIZE-1)-WIDTH*(0+1)+1];
+		update_value[0] <= update_value[0] + in[(WIDTH*CHUNK_SIZE-1)-WIDTH*0:(WIDTH*CHUNK_SIZE-1)-WIDTH*(0+1)+1]; // in[(64-1)-0:(64-1)-16*1+1]= [63:48]
         update_value[1] <= update_value[1] + in[(WIDTH*CHUNK_SIZE-1)-WIDTH*1:(WIDTH*CHUNK_SIZE-1)-WIDTH*(1+1)+1];
         update_value[2] <= update_value[2] + in[(WIDTH*CHUNK_SIZE-1)-WIDTH*2:(WIDTH*CHUNK_SIZE-1)-WIDTH*(2+1)+1];
         update_value[3] <= update_value[3] + in[(WIDTH*CHUNK_SIZE-1)-WIDTH*3:(WIDTH*CHUNK_SIZE-1)-WIDTH*(3+1)+1];
