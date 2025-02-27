12c601cbb3ba9b9b731afa2b8d88e17cb70e2afb
fauzan-2405 <fauzaanmuhammaad@gmail.com>
2025-02-27 14:45:27 +0700

    #1 massive update for sys array: not using negedge clk

--- matrix_mult/pe.v
+++ matrix_mult/pe.v
@@ -15,19 +15,32 @@ module pe #(
     wire [2*WIDTH-1:0] mult_result;
     wire [2*WIDTH-1:0] add_result;
     wire [WIDTH-1:0] temp_acc;
-
-    always @(posedge clk) begin
+	
+	reg count = 1'b0;
+	
+	/*
+    always @(negedge clk) begin
         if (!rst_n) begin
             out_south <= 0;
             out_east <= 0;
             result <= 0;
-        end 
-        else begin
-            out_east <= in_west;
-            out_south <= in_north;
-            result <= temp_acc;
+        end
+	end
+	*/
+	
+	always @(posedge clk) begin
+        if (!rst_n) begin
+            out_south <= 0;
+            out_east <= 0;
+            result <= 0;
+        end
+		else begin
+			out_east <= in_west;
+			out_south <= in_north;
+			result <= temp_acc;
         end
     end
+	
 
 	assign mult_result = {{WIDTH{in_west[WIDTH-1]}}, in_west} * {{WIDTH{in_north[WIDTH-1]}}, in_north};
     assign add_result = mult_result + {{FRAC_WIDTH{result[WIDTH-1]}}, result, {FRAC_WIDTH{1'b0}}};
