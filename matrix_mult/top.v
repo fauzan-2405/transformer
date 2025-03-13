761c6caf5cb678ef6d0467ec01bb739d03e240f8
fauzan-2405 <fauzaanmuhammaad@gmail.com>
2025-03-13 13:15:55 +0700

    Update for input & output ports

--- matrix_mult/top.v
+++ matrix_mult/top.v
@@ -8,16 +8,31 @@ module top #(
     parameter FRAC_WIDTH = 8,
     parameter BLOCK_SIZE = 2, // The size of systolic array dimension (N x N)
     parameter CHUNK_SIZE = 4,
-    parameter INNER_DIMENSION = 64 // The same number of rows in one matrix and same number of columns in the other matrix
+    parameter INNER_DIMENSION = 256, // The same number of rows in one matrix and same number of columns in the other matrix
+    // W stands for weight
+    parameter W_OUTER_DIMENSION = 64,
+    // I stands for input
+    parameter I_OUTER_DIMENSION = 2754
 ) (
     input clk, rst_n, en, clr,
     // Control and status port
-    output  ready,
-    input   start,
-    output  done,
+    output ready,
+    input  start,
+    output done,
     // Weight port
-    input   wb_en,
-    input   wb_addr,
-    input   
-
-)
+    // For weight, there is 256x64 data with 16 bits each
+    input wb_en,
+    input [(INNER_DIMENSION/CHUNK_SIZE)*W_OUTER_DIMENSION-1:0] wb_addr, // 256/4 = 64 x 256
+    input [WIDTH*CHUNK_SIZE-1:0] wb_din,
+    input [7:0] wb_we,
+    // Data input port
+    input in_en,
+    input [(INNER_DIMENSION/CHUNK_SIZE)*I_OUTER_DIMENSION-1:0] in_addr,
+    input [WIDTH*CHUNK_SIZE-1:0] in_din,
+    input [7:0] in_we,
+    // Data output port
+    input out_en,
+    input [(W_OUTER_DIMENSION/CHUNK_SIZE)*I_OUTER_DIMENSION],
+    output [WIDTH*CHUNK_SIZE-1:0] out_dout
+);
+endmodule
