554a485c53047060d38f3e606c80c8a71d559968
fauzan-2405 <fauzaanmuhammaad@gmail.com>
2025-03-19 11:14:40 +0700

    update enable logic

--- matrix_mult/Multi MAC/systolic_array_2x2.v
+++ matrix_mult/Multi MAC/systolic_array_2x2.v
@@ -17,8 +17,8 @@ module systolic_array_2x2 #(
     reg [2:0] count = 3'b111;
 
     // For convenience
-    wire [WIDTH-1:0] in_north0, in_north1;
-    wire [WIDTH-1:0] in_west0, in_west2;
+    //wire [WIDTH-1:0] in_north0, in_north1;
+    //wire [WIDTH-1:0] in_west0, in_west2;
     wire [WIDTH-1:0] out_south0, out_south1, out_south2, out_south3;
     wire [WIDTH-1:0] out_east0, out_east1, out_east2, out_east3;
     wire [WIDTH-1:0] result0, result1, result2, result3;
@@ -34,19 +34,21 @@ module systolic_array_2x2 #(
     assign out = {result0, result1, result2, result3};
 
     always @(posedge clk) begin
-        if (!rst_n || en) begin
+        if (!rst_n) begin
             done <= 0;
             count <= 0;
         end
         else begin
-            if (count == 4) begin
-                done <= 1;
-                count <= 0;
-            end
-            else begin
-                count <= count + 1;
-                done <= 0;
-            end
+			if (en) begin
+			    if (count == 4) begin
+					done <= 1;
+					count <= 0;
+				end
+				else begin
+					count <= count + 1;
+					done <= 0;
+				end
+			end
         end
     end
 endmodule
