2822d8203ee3170a9513d205f4c57471eddf0c24
fauzan-2405 <fauzaanmuhammaad@gmail.com>
2025-03-19 11:13:51 +0700

    update mux enable logic

--- matrix_mult/Multi MAC/mux2_1.v
+++ matrix_mult/Multi MAC/mux2_1.v
@@ -4,12 +4,12 @@
 module mux2_1 #(
     parameter WIDTH = 16
 ) (
-    input clk, rst_n,
+    input clk, rst_n, en,
     input [WIDTH-1:0] input_0, input_1, 
     output reg [WIDTH-1:0] out
 );
     reg select = 1'b0;
-    // reg select;
+    //reg select;
 	
     always @(posedge clk) begin
         if (!rst_n) begin
@@ -17,11 +17,13 @@ module mux2_1 #(
             select <= 1'd0;
         end
         else begin
-            case (select)
-            1'b0 : out <= input_0;
-            1'b1 : out <= input_1;
-            endcase
-			select <= select+1;
+			if (en) begin
+				case (select)
+				1'b0 : out <= input_0;
+				1'b1 : out <= input_1;
+				endcase
+				select <= select+1;
+			end
         end
     end
 
