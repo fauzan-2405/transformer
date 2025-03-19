f9fbda587bf4da65a5ec48bf931e88295e7427e3
fauzan-2405 <fauzaanmuhammaad@gmail.com>
2025-03-19 11:13:12 +0700

    update cmux

--- matrix_mult/Multi MAC/control_mux2_1.v
+++ matrix_mult/Multi MAC/control_mux2_1.v
@@ -2,19 +2,21 @@
 // Used to control mux to stream input
 
 module control_mux2_1 (
-    input clk, rst_n,
+    input clk, rst_n, en,
     output wire [1:0] mux_reset
 );
-    reg [3:0] counter = 4'b10_01; // For controlling reset
-	//reg [3:0] counter;
-    assign mux_reset = counter[3:2];
+    //reg [3:0] counter = 4'b10_01; // For controlling reset
+	reg [3:0] counter;
+	assign mux_reset = counter[3:2];
 
     always @(posedge clk) begin
         if (rst_n) begin // Active
 			if (counter == 4'b00_11) begin
 				counter <= 4'b00_11;
 			end else begin
-				counter <= (counter >> 1)|(counter << 3);
+				if (en) begin
+					counter <= (counter >> 1)|(counter << 3);
+				end
 			end
         end
         else begin // Not Active
