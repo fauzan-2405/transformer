eaee4e1e99a30e9ec025710a8d9987262ed7a949
fauzan-2405 <fauzaanmuhammaad@gmail.com>
2025-04-21 14:03:02 +0700

    reduced the logic for generating the cores

--- matrix_mult/Multi MAC/toplevel_v2.v
+++ matrix_mult/Multi MAC/toplevel_v2.v
@@ -35,48 +35,14 @@ module toplevel_v2 #(
     
 	// Core generation
 	generate
-		case (NUM_CORES)
-			17: for (i = 0; i < 17; i = i +1) begin
-				core #(.WIDTH(WIDTH), .FRAC_WIDTH(FRAC_WIDTH), .BLOCK_SIZE(BLOCK_SIZE), .CHUNK_SIZE(CHUNK_SIZE), .INNER_DIMENSION(INNER_DIMENSION)) core_17 (
-					.clk(clk), .en(en), .rst_n(rst_n), .reset_acc(reset_acc),
-                    .input_w(input_w_array[i]), .input_n(input_n),
-                    .accumulator_done(acc_done_array[i]), .systolic_finish(systolic_finish_array[i]),
-                    .out(out_top[(i+1)*(WIDTH*CHUNK_SIZE)-1 -: (WIDTH*CHUNK_SIZE)])
-				);
-				  end
-			8:  for (i = 0; i < 8; i = i +1) begin
-				core #(.WIDTH(WIDTH), .FRAC_WIDTH(FRAC_WIDTH), .BLOCK_SIZE(BLOCK_SIZE), .CHUNK_SIZE(CHUNK_SIZE), .INNER_DIMENSION(INNER_DIMENSION)) core_8 (
-					.clk(clk), .en(en), .rst_n(rst_n), .reset_acc(reset_acc),
-                    .input_w(input_w_array[i]), .input_n(input_n),
-                    .accumulator_done(acc_done_array[i]), .systolic_finish(systolic_finish_array[i]),
-                    .out(out_top[(i+1)*(WIDTH*CHUNK_SIZE)-1 -: (WIDTH*CHUNK_SIZE)])		
-				);
-				  end
-			5:  for (i = 0; i < 5; i = i +1) begin
-				core #(.WIDTH(WIDTH), .FRAC_WIDTH(FRAC_WIDTH), .BLOCK_SIZE(BLOCK_SIZE), .CHUNK_SIZE(CHUNK_SIZE), .INNER_DIMENSION(INNER_DIMENSION)) core_5 (
-					.clk(clk), .en(en), .rst_n(rst_n), .reset_acc(reset_acc),
-                    .input_w(input_w_array[i]), .input_n(input_n),
-                    .accumulator_done(acc_done_array[i]), .systolic_finish(systolic_finish_array[i]),
-                    .out(out_top[(i+1)*(WIDTH*CHUNK_SIZE)-1 -: (WIDTH*CHUNK_SIZE)])					
-				);
-				  end
-			4:   for (i = 0; i < 4; i = i +1) begin
-				core #(.WIDTH(WIDTH), .FRAC_WIDTH(FRAC_WIDTH), .BLOCK_SIZE(BLOCK_SIZE), .CHUNK_SIZE(CHUNK_SIZE), .INNER_DIMENSION(INNER_DIMENSION)) core_4 (
-					.clk(clk), .en(en), .rst_n(rst_n), .reset_acc(reset_acc),
-                    .input_w(input_w_array[i]), .input_n(input_n),
-                    .accumulator_done(acc_done_array[i]), .systolic_finish(systolic_finish_array[i]),
-                    .out(out_top[(i+1)*(WIDTH*CHUNK_SIZE)-1 -: (WIDTH*CHUNK_SIZE)])				
-				);
-				  end
-            default: for (i = 0; i < 2; i = i +1) begin
-                core #(.WIDTH(WIDTH), .FRAC_WIDTH(FRAC_WIDTH), .BLOCK_SIZE(BLOCK_SIZE), .CHUNK_SIZE(CHUNK_SIZE), .INNER_DIMENSION(INNER_DIMENSION)) core_4 (
-					.clk(clk), .en(en), .rst_n(rst_n), .reset_acc(reset_acc),
-                    .input_w(input_w_array[i]), .input_n(input_n),
-                    .accumulator_done(acc_done_array[i]), .systolic_finish(systolic_finish_array[i]),
-                    .out(out_top[(i+1)*(WIDTH*CHUNK_SIZE)-1 -: (WIDTH*CHUNK_SIZE)])				
-				);
-                  end
-		endcase
+        for (i = 0; i < NUM_CORES; i = i +1) begin
+            core #(.WIDTH(WIDTH), .FRAC_WIDTH(FRAC_WIDTH), .BLOCK_SIZE(BLOCK_SIZE), .CHUNK_SIZE(CHUNK_SIZE), .INNER_DIMENSION(INNER_DIMENSION)) core_inst (
+                .clk(clk), .en(en), .rst_n(rst_n), .reset_acc(reset_acc),
+                .input_w(input_w_array[i]), .input_n(input_n),
+                .accumulator_done(acc_done_array[i]), .systolic_finish(systolic_finish_array[i]),
+                .out(out_top[(i+1)*(WIDTH*CHUNK_SIZE)-1 -: (WIDTH*CHUNK_SIZE)])				
+            );
+        end
 	endgenerate
 	
     assign accumulator_done = &acc_done_array[NUM_CORES-1:0];
