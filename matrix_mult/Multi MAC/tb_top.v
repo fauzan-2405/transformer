118bbc14b87421110deb2c0be2d4a1ba95edddf5
fauzan-2405 <fauzaanmuhammaad@gmail.com>
2025-04-30 15:12:13 +0700

    updated tb_top with clog2 function

--- matrix_mult/Multi MAC/tb_top.v
+++ matrix_mult/Multi MAC/tb_top.v
@@ -3,6 +3,16 @@
 `timescale 1ns / 1ps
 
 module tb_top;
+// clog2 function
+function integer clog2;
+    input integer value;
+    begin  
+        value = value-1;
+        for (clog2=0; value>0; clog2=clog2+1)
+        value = value>>1;
+    end
+endfunction
+
 parameter WIDTH = 16;
 parameter FRAC_WIDTH = 8;
 parameter BLOCK_SIZE = 2; // The size of systolic array dimension (N x N)
@@ -23,6 +33,8 @@ parameter NUM_CORES = (INNER_DIMENSION == 2754) ? 9 :
                                (INNER_DIMENSION == 200)  ? 5 :
                                (INNER_DIMENSION == 64)   ? 4 : 2;
 parameter MAX_FLAG = ROW_SIZE_MAT_C * COL_SIZE_MAT_C / NUM_CORES;
+parameter ADDR_WIDTH_I = clog2((INNER_DIMENSION*I_OUTER_DIMENSION*WIDTH)/(WIDTH*CHUNK_SIZE*NUM_CORES)); // Used for determining the width of wb and input address and other parameters in BRAMs
+parameter ADDR_WIDTH_W = clog2((INNER_DIMENSION*W_OUTER_DIMENSION*WIDTH)/(WIDTH*CHUNK_SIZE));
 
 reg clk;
 reg rst_n;
@@ -30,12 +42,12 @@ reg rst_n;
 reg start;
 
 reg wb_ena;
-reg [11:0] wb_addra;
+reg [ADDR_WIDTH_W-1:0] wb_addra;
 reg [WIDTH*CHUNK_SIZE-1:0] wb_dina;
 reg [7:0] wb_wea;
 
 reg in_ena;
-reg [13:0] in_addra;
+reg [ADDR_WIDTH_I-1:0] in_addra;
 reg [WIDTH*CHUNK_SIZE*NUM_CORES-1:0] in_dina;
 reg [7:0] in_wea;
 
@@ -48,7 +60,8 @@ wire [(WIDTH*CHUNK_SIZE*NUM_CORES)-1:0] out_bram; // For top_v2.v
 top #( // DONT FORGET TO CHANGE THIS TOO
     .WIDTH(WIDTH), .FRAC_WIDTH(FRAC_WIDTH), .BLOCK_SIZE(BLOCK_SIZE), .CHUNK_SIZE(CHUNK_SIZE),
     .INNER_DIMENSION(INNER_DIMENSION), .W_OUTER_DIMENSION(W_OUTER_DIMENSION), .I_OUTER_DIMENSION(I_OUTER_DIMENSION), 
-    .ROW_SIZE_MAT_C(ROW_SIZE_MAT_C), .COL_SIZE_MAT_C(COL_SIZE_MAT_C), .NUM_CORES(NUM_CORES), .MAX_FLAG(MAX_FLAG)
+    .ROW_SIZE_MAT_C(ROW_SIZE_MAT_C), .COL_SIZE_MAT_C(COL_SIZE_MAT_C), .NUM_CORES(NUM_CORES), .MAX_FLAG(MAX_FLAG),
+    .ADDR_WIDTH_W(ADDR_WIDTH_W), .ADDR_WIDTH_I(ADDR_WIDTH_I)
 ) top_inst ( 
     .clk(clk),
     .rst_n(rst_n),
