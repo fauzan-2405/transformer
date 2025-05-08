3ce5632ae14ccdcbfb08dcbeba92eb0d95f83dbe
fauzan-2405 <fauzaanmuhammaad@gmail.com>
2025-05-08 16:27:28 +0700

    added leading one detection module

---
+++ softmax/lod.v
@@ -0,0 +1,18 @@
+// lod.v
+// This module is used for detecting the leading one (Leading One Detection)
+
+module lod (
+    input  wire [31:0] num,
+    output reg  [4:0]  pos
+);
+    integer i;
+    always @(*) begin
+        pos = 5'd0;
+        for (i = 31; i >= 0; i = i - 1) begin
+            if (num[i] == 1'b1) begin
+                pos = i[4:0];
+                disable for_loop;
+            end
+        end
+    end
+endmodule
