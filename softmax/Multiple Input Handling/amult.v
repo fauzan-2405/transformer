c108dc8312968353d04c5e3cd74b42912965f92e
fauzan-2405 <fauzaanmuhammaad@gmail.com>
2025-08-15 10:43:24 +0700

    minor update

--- softmax/Multiple Input Handling/amult.v
+++ softmax/Multiple Input Handling/amult.v
@@ -1,24 +1,3 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 01/07/2025 03:08:55 PM
-// Design Name: 
-// Module Name: amult
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module amult #(
 //Approximate Multiplier
