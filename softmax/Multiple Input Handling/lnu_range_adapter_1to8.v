3b5598f109293a48e941e34a54f1ae203048a17d
fauzan-2405 <fauzaanmuhammaad@gmail.com>
2025-08-25 14:09:22 +0700

    refactor lnu_range_adapter_1to8: improve leading one detection, normalize input, and streamline calculations

--- softmax/Multiple Input Handling/lnu_range_adapter_1to8.v
+++ softmax/Multiple Input Handling/lnu_range_adapter_1to8.v
@@ -8,6 +8,8 @@ module lnu_range_adapter_1to8 #(
     input wire [WIDTH+16-1:0] x_sum_exp,
     output wire [WIDTH-1:0] y_ln_out
 );
+    // Constants
+    localparam integer SUMW = WIDTH + 16;
     wire [WIDTH-1:0] ln2_q    = 32'h0000B172; // ~0.693147 in Q16.16
     
     // Clamp non-positive value
@@ -15,42 +17,42 @@ module lnu_range_adapter_1to8 #(
                                     {{(WIDTH+16-FRAC-1){1'b0}}, 1'b1, {FRAC{1'b0}}} : // ~1.0 in Q16.16
                                     x_sum_exp;
     
-    // Finding the k so sum_exp = m * 2^k
-    // Reduce sum_exp to 32-bit Q16.16
-    localparam integer SUMW = WIDTH + 16;
-    wire [31:0] x32 = (SUMW > 32) ? x_clamped[SUMW-1 -: 32] : { {(32-SUMW){1'b0}}, x_clamped };
+    // Leading one detector
+    integer i;
+    reg [$clog2(SUMW)-1:0] lead_one_pos;
+    always @(*) begin
+        lead_one_pos = 0;
+        for (i = SUMW-1; i >= 0; i = i-1) begin
+            if (x_clamped[i]) begin
+                lead_one_pos = i;
+                disable for; // break after first one
+            end
+        end
+    end
 
-    // Bring x32 into [1,8) by shifting and compute how many bits to shift to get integer part in [1 ... 7]
-    wire [WIDTH-1:0] int_part = x32[(2*WIDTH)-1:WIDTH];
-    integer k_signed;
-    reg [2*WIDTH-1:0] m_q;
+    // Normalizing, Target: integer part of m is in [1,7], i.e. m in [1,8).
+    reg signed [15:0] k_shift;
+    reg [WIDTH-1:0]   m_norm;
 
     always @(*) begin
-        k_signed = 0;
-        m_q      = x32;
-
-        // scaling it down if > 8.0
-        while (m_q[(2*WIDTH)-1:WIDTH] >= 16'd8) begin
-            m_q      = {1'b0, m_q[31:1]}; // divide by 2
-            k_signed = k_signed + 1;
-        end
-
-        // scaling it up if < 1.0
-        while (m_q[(2*WIDTH)-1:WIDTH] < 16'd1) begin
-            m_q      = {m_q[30:0], 1'b0}; // multiply by 2
-            k_signed = k_signed - 1;
+        if (lead_one_pos > (FRAC+2)) begin
+            k_shift = lead_one_pos - (FRAC+2);
+            m_norm  = x_clamped >> k_shift;
+        end else begin
+            k_shift = -((FRAC+2) - lead_one_pos);
+            m_norm  = x_clamped << ((FRAC+2) - lead_one_pos);
         end
     end
 
     // Calling the LNU
-    wire [31:0] ln_m;
+    wire [WIDTH-1:0] ln_m;
     lnu LNU (
-        .x_in(m_q), .ln_out(ln_m)
+        .x_in(m_norm), .ln_out(ln_m)
     );
 
     // ln(sum_exp) = ln(m_q) + k*ln(2)
-    wire [63:0] k_ln2_full = $signed(k_signed) * $signed({{16{ln2_q[31]}}, ln2_q}); // extend to 48b then multiply
-    wire [31:0] k_ln2_q    = k_ln2_full[31:0]; // ok for modest |k|
+    wire [63:0] k_ln2_full = $signed(k_shift) * $signed({{16{ln2_q[31]}}, ln2_q});
+    wire [WIDTH-1:0] k_ln2_q = k_ln2_full[WIDTH-1:0];
 
     assign y_ln_out = ln_m + k_ln2_q;
 
