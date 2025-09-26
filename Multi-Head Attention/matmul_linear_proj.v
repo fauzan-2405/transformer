f66bc14536d3d74890352c1930506392cbf4186d
fauzan-2405 <fauzaanmuhammaad@gmail.com>
2025-09-26 10:13:55 +0700

    first commit

--- Multi-Head Attention/matmul_linear_proj.v
+++ Multi-Head Attention/matmul_linear_proj.v
@@ -1,12 +1,12 @@
-// linear_projection.v
+// matmul_linear_proj.v
 // Used to do linear projection for Q, K, and V
 
-module linear_projection #(
+module matmul_linear_proj #(
     parameter siuuuu = 2
 ) (
     input clk,
     input rst_n
 );
     localparam euyyyy = 3;
-    
+
 endmodule
