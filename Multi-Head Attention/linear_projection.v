af6285829a0dde27dc5acd4423b64840fa564678
fauzan-2405 <fauzaanmuhammaad@gmail.com>
2025-09-26 09:34:19 +0700

    first commit

---
+++ Multi-Head Attention/linear_projection.v
@@ -0,0 +1,12 @@
+// linear_projection.v
+// Used to do linear projection for Q, K, and V
+
+module linear_projection #(
+    parameter siuuuu = 2
+) (
+    input clk,
+    input rst_n
+);
+    localparam euyyyy = 3;
+    
+endmodule
