bd1456bcc4006772a3e70d73241f9f3eaa215f11
fauzan-2405 <fauzaanmuhammaad@gmail.com>
2025-10-06 15:08:47 +0700

    fix minor errors

--- Multi-Head Attention/linear_proj_pkg.sv
+++ Multi-Head Attention/linear_proj_pkg.sv
@@ -14,17 +14,17 @@ package linear_proj_pkg;
     parameter int BLOCK_SIZE     = 2; 
     parameter int CHUNK_SIZE     = 4;
 
-    parameter int A_OUTER_DIMENSION = 6;
-    parameter int B_OUTER_DIMENSION = 6;
-    parameter int INNER_DIMENSION= 64;
+    parameter int A_OUTER_DIMENSION = 8;
+    parameter int B_OUTER_DIMENSION = 8;
+    parameter int INNER_DIMENSION= 6;
     parameter int NUM_CORES_B    = 1;
     parameter int NUM_CORES_A    = 2;
 
-    parameter TOTAL_INPUT_W = 2,
-    parameter TOTAL_MODULES = 4
+    parameter TOTAL_INPUT_W = 2;
+    parameter TOTAL_MODULES = 4;
     
-    parameter int NUM_A_ELEMENTS = ((I_OUTER_DIMENSION/BLOCK_SIZE)*(INNER_DIMENSION/BLOCK_SIZE))/(NUM_CORES_A*TOTAL_INPUT_W); // Total elements of Input if we converted the inputs based on the NUM_CORES
-    parameter int NUM_B_ELEMENTS = ((W_OUTER_DIMENSION/BLOCK_SIZE)*(INNER_DIMENSION/BLOCK_SIZE))/(NUM_CORES_B*TOTAL_MODULES);
+    parameter int NUM_A_ELEMENTS = ((A_OUTER_DIMENSION/BLOCK_SIZE)*(INNER_DIMENSION/BLOCK_SIZE))/(NUM_CORES_A*TOTAL_INPUT_W); // Total elements of Input if we converted the inputs based on the NUM_CORES
+    parameter int NUM_B_ELEMENTS = ((B_OUTER_DIMENSION/BLOCK_SIZE)*(INNER_DIMENSION/BLOCK_SIZE))/(NUM_CORES_B*TOTAL_MODULES);
 
     parameter int ROW_SIZE_MAT_C = A_OUTER_DIMENSION / (BLOCK_SIZE * NUM_CORES_A * TOTAL_INPUT_W); 
     parameter int COL_SIZE_MAT_C = B_OUTER_DIMENSION / (BLOCK_SIZE * NUM_CORES_B * TOTAL_MODULES); 
