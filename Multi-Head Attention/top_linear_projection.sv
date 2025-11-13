0f92a666a19faa774575511d558eade7215c4283
fauzan-2405 <fauzaanmuhammaad@gmail.com>
2025-11-13 19:39:51 +0700

    initial commit

---
+++ Multi-Head Attention/top_linear_projection.sv
@@ -0,0 +1,12 @@
+// top_linear_projection.sv
+// Top module for linear_projection (keys + matmul modules) + controller + input BRAM
+import linear_proj_pkg::*;
+
+module top_linear_projection #(
+    parameter XXXX = yy
+) (
+    input logic clk,
+    output logic xxx
+);
+
+endmodule
