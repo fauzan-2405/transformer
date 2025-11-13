3114c77db8afddbb2ceb70cb75a4340385e17448
fauzan-2405 <fauzaanmuhammaad@gmail.com>
2025-11-13 14:34:04 +0700

    initial_commit

---
+++ Multi-Head Attention/linear_projection.sv
@@ -0,0 +1,13 @@
+// linear_projection.sv
+// Used to do linear projection of course (duh)
+import linear_proj_pkg::*;
+
+module linear_pojection #(
+    parameter XXX = YY
+) (
+    input logic xxx,
+    output logic yyy
+);
+    // Bismillah
+
+endmodule
