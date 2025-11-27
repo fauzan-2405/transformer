6993a23df27f2107bdf1db82bf15087b7070a243
fauzan-2405 <fauzaanmuhammaad@gmail.com>
2025-11-27 16:36:40 +0700

    completed the parameters and ports for matmul module

--- Multi-Head Attention/Self-Attention Head/self_attention_head.sv
+++ Multi-Head Attention/Self-Attention Head/self_attention_head.sv
@@ -7,9 +7,11 @@ module self_attention_head #(
 ) (
     input clk, rst_n,
     input enable_self_attention,
-    input 
+    input reset_acc_self_attention,
+    input logic [(WIDTH_A*CHUNK_SIZE*NUM_CORES_A)-1:0] input_w_qkt [TOTAL_INPUT_W],
+    input logic [(WIDTH_B*CHUNK_SIZE*NUM_CORES_B*TOTAL_MODULES)-1:0] input_n_qkt, 
 
-    output XXXX
+    output sys_finish_wrap_qkt, acc_done_wrap_qkt
 );
     // ************************** Matmul Module Qn x Kn^T **************************
     multi_matmul_wrapper #(
@@ -22,19 +24,19 @@ module self_attention_head #(
         .BLOCK_SIZE(BLOCK_SIZE),
         .CHUNK_SIZE(CHUNK_SIZE),
         .INNER_DIMENSION(INNER_DIMENSION_Qn_KnT),
-        .TOTAL_MODULES(TOTAL_MODULES_LP),
+        .TOTAL_MODULES(TOTAL_MODULES),
         .TOTAL_INPUT_W(TOTAL_INPUT_W_Qn_KnT),
         .NUM_CORES_A(NUM_CORES_A_Qn_KnT),
         .NUM_CORES_B(NUM_CORES_B_Qn_KnT)
     ) matmul_Qn_KnT (
         .clk(clk),
         .rst_n(rst_n),
-        .en(),
-        .reset_acc(),
-        .input_w(), 
-        .input_n(), 
-        .acc_done_wrap(), 
-        .systolic_finish_wrap(),
+        .en(enable_self_attention),
+        .reset_acc(reset_acc_self_attention),
+        .input_w(input_w_qkt), 
+        .input_n(input_n_qkt), 
+        .acc_done_wrap(acc_done_wrap_qkt), 
+        .systolic_finish_wrap(sys_finish_wrap_qkt),
         .out_multi_matmul()
     );
 
