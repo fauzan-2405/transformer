3a48fc36cd6228ef71982292e5124e82bc2ba750
fauzan-2405 <fauzaanmuhammaad@gmail.com>
2025-12-01 16:13:04 +0700

    updated with 4-bit shfter instance

--- Multi-Head Attention/Self-Attention Head/self_attention_head.sv
+++ Multi-Head Attention/Self-Attention Head/self_attention_head.sv
@@ -14,6 +14,8 @@ module self_attention_head #(
     output sys_finish_wrap_qkt, acc_done_wrap_qkt
 );
     // ************************** Matmul Module Qn x Kn^T **************************
+    logic [(WIDTH_OUT*CHUNK_SIZE*NUM_CORES_A*NUM_CORES_B*TOTAL_MODULES_LP_Q)-1:0] out_matmul_qkt [TOTAL_INPUT_W_Qn_KnT]
+
     multi_matmul_wrapper #(
         .WIDTH_A(WIDTH_A),
         .FRAC_WIDTH_A(FRAC_WIDTH_A),
@@ -24,7 +26,7 @@ module self_attention_head #(
         .BLOCK_SIZE(BLOCK_SIZE),
         .CHUNK_SIZE(CHUNK_SIZE),
         .INNER_DIMENSION(INNER_DIMENSION_Qn_KnT),
-        .TOTAL_MODULES(TOTAL_MODULES),
+        .TOTAL_MODULES(TOTAL_MODULES_LP_Q),
         .TOTAL_INPUT_W(TOTAL_INPUT_W_Qn_KnT),
         .NUM_CORES_A(NUM_CORES_A_Qn_KnT),
         .NUM_CORES_B(NUM_CORES_B_Qn_KnT)
@@ -37,10 +39,33 @@ module self_attention_head #(
         .input_n(input_n_qkt), 
         .acc_done_wrap(acc_done_wrap_qkt), 
         .systolic_finish_wrap(sys_finish_wrap_qkt),
-        .out_multi_matmul()
+        .out_multi_matmul(out_matmul_qkt)
     );
 
     // ************************** 4-BIT SHIFTER **************************
+    logic [(WIDTH_OUT*CHUNK_SIZE*NUM_CORES_A*NUM_CORES_B*TOTAL_MODULES_LP_Q)-1:0] out_shifted [TOTAL_INPUT_W_Qn_KnT] 
+
+    4bit_rshift #(
+        .WIDTH_A(WIDTH_A),
+        .FRAC_WIDTH_A(FRAC_WIDTH_A),
+        .WIDTH_B(WIDTH_B),
+        .FRAC_WIDTH_B(FRAC_WIDTH_B),
+        .WIDTH_OUT(WIDTH_OUT),
+        .FRAC_WIDTH_OUT(FRAC_WIDTH_OUT),
+        .BLOCK_SIZE(BLOCK_SIZE),
+        .CHUNK_SIZE(CHUNK_SIZE),
+        .TOTAL_MODULES(TOTAL_MODULES_LP_Q),
+        .TOTAL_INPUT_W(TOTAL_INPUT_W_Qn_KnT),
+        .NUM_CORES_A(NUM_CORES_A_Qn_KnT),
+        .NUM_CORES_B(NUM_CORES_B_Qn_KnT)
+    ) 4bit_rshift (
+        .clk(clk), .rst_n(rst_n),
+        .in_4bit_rshift(out_matmul_qkt),
+        .out_shifted(out_shifted)
+    );
+
+    
+    
 
 
 endmodule
