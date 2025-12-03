8203e52a350fe89c4759569d8445c713a7ebd6ae
fauzan-2405 <fauzaanmuhammaad@gmail.com>
2025-12-03 14:29:49 +0700

    some minor bug fixes

--- Multi-Head Attention/top_pkg.sv
+++ Multi-Head Attention/top_pkg.sv
@@ -2,15 +2,15 @@
 // This used as the top level of the parameter package for the entire multi-head-attention
 
 package top_pkg;
-    parameter int WIDTH_A        = 16;
-    parameter int FRAC_WIDTH_A   = 8;
-    parameter int WIDTH_B        = 16;
-    parameter int FRAC_WIDTH_B   = 8;
-    parameter int WIDTH_OUT      = 16;
-    parameter int FRAC_WIDTH_OUT = 8;
+    parameter int TOP_WIDTH_A        = 16;
+    parameter int TOP_FRAC_WIDTH_A   = 8;
+    parameter int TOP_WIDTH_B        = 16;
+    parameter int TOP_FRAC_WIDTH_B   = 8;
+    parameter int TOP_WIDTH_OUT      = 16;
+    parameter int TOP_FRAC_WIDTH_OUT = 8;
 
-    parameter int BLOCK_SIZE     = 2; 
-    parameter int CHUNK_SIZE     = 4;
+    parameter int TOP_BLOCK_SIZE     = 2; 
+    parameter int TOP_CHUNK_SIZE     = 4;
 
     parameter TOTAL_MODULES_K    = 4; // N parameters
     parameter TOTAL_MODULES_Q    = 4; // P parameters
