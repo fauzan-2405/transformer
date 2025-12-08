83295786224a000a5247f8813b8f47b693b036d7
fauzan-2405 <fauzaanmuhammaad@gmail.com>
2025-12-08 11:30:44 +0700

    minor update

--- Multi-Head Attention/Self-Attention Head/self_attention_head.sv
+++ Multi-Head Attention/Self-Attention Head/self_attention_head.sv
@@ -14,7 +14,8 @@ module self_attention_head #(
     output sys_finish_wrap_qkt, acc_done_wrap_qkt
 );
     // ************************** Matmul Module Qn x Kn^T **************************
-    logic [(WIDTH_OUT*CHUNK_SIZE*NUM_CORES_A*NUM_CORES_B*TOTAL_MODULES_LP_Q)-1:0] out_matmul_qkt [TOTAL_INPUT_W_Qn_KnT]
+    logic [(WIDTH_OUT*CHUNK_SIZE*NUM_CORES_A*NUM_CORES_B*TOTAL_MODULES_LP_Q)-1:0] out_matmul_qkt [TOTAL_INPUT_W_Qn_KnT];
+    logic sig_acc_done_wrap_qkt;
 
     multi_matmul_wrapper #(
         .WIDTH_A(WIDTH_A),
@@ -37,15 +38,17 @@ module self_attention_head #(
         .reset_acc(reset_acc_self_attention),
         .input_w(input_w_qkt), 
         .input_n(input_n_qkt), 
-        .acc_done_wrap(acc_done_wrap_qkt), 
+        .acc_done_wrap(sig_acc_done_wrap_qkt), 
         .systolic_finish_wrap(sys_finish_wrap_qkt),
         .out_multi_matmul(out_matmul_qkt)
     );
 
-    // ************************** 4-BIT SHIFTER **************************
-    logic [(WIDTH_OUT*CHUNK_SIZE*NUM_CORES_A*NUM_CORES_B*TOTAL_MODULES_LP_Q)-1:0] out_shifted [TOTAL_INPUT_W_Qn_KnT] 
+    assign acc_done_wrap_qkt = sig_acc_done_wrap_qkt;
 
-    4bit_rshift #(
+    // ************************** 4-BIT SHIFTER **************************
+    logic [(WIDTH_OUT*CHUNK_SIZE*NUM_CORES_A*NUM_CORES_B*TOTAL_MODULES_LP_Q)-1:0] out_shifted [TOTAL_INPUT_W_Qn_KnT];
+
+    rshift #(
         .WIDTH_A(WIDTH_A),
         .FRAC_WIDTH_A(FRAC_WIDTH_A),
         .WIDTH_B(WIDTH_B),
@@ -58,13 +61,15 @@ module self_attention_head #(
         .TOTAL_INPUT_W(TOTAL_INPUT_W_Qn_KnT),
         .NUM_CORES_A(NUM_CORES_A_Qn_KnT),
         .NUM_CORES_B(NUM_CORES_B_Qn_KnT)
-    ) 4bit_rshift (
+    ) rshift_four_bit (
         .clk(clk), .rst_n(rst_n),
+        .in_valid(sig_acc_done_wrap_qkt),
         .in_4bit_rshift(out_matmul_qkt),
+        .out_valid()
         .out_shifted(out_shifted)
     );
 
-    
+    // ************************** B2R CONVERTER **************************
     
 
 
