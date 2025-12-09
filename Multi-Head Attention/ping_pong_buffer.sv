0fd93e97398a92e33ca51340e0f278a9e6b2fc2b
fauzan-2405 <fauzaanmuhammaad@gmail.com>
2025-12-09 15:00:00 +0700

    minor update

--- Multi-Head Attention/ping_pong_buffer.sv
+++ Multi-Head Attention/ping_pong_buffer.sv
@@ -1,12 +1,149 @@
 // ping_pong_buffer.sv
-// Used to bridge linear projection results with Qn x KnT matmul in self-head attention
-import linear_proj_pkg::*;
+// Used to bridge linear projection results with Qn x KnT matmul in self-head attention (or other things)
+// The input consists NUM_CORES_A * NUM_CORES_B * TOTAL_MODULES blocks
 
 module ping_pong_buffer #(
-    parameter IN_WIDTH   = WIDTH_OUT*CHUNK_SIZE*NUM_CORES_A*NUM_CORES_B*TOTAL_MODULES
+    parameter WIDTH             = 16,
+    parameter NUM_CORES_A       = 2,
+    parameter NUM_CORES_B       = 1,
+    parameter TOTAL_MODULES     = 4,
+    parameter COL_X             = 16, // COL SIZE of matrix X (producer) 
+    parameter COL_Y             = 16, // COL SIZE of matrix y (consumer) 
+    parameter TOTAL_INPUT_W     = 2,
+
+    localparam CHUNK_SIZE       = top_pkg::TOP_CHUNK_SIZE,
+    localparam BLOCK_SIZE       = top_pkg::TOP_BLOCK_SIZE,
+    localparam MODULE_WIDTH     = WIDTH*CHUNK_SIZE*NUM_CORES_A*NUM_CORES_B,
+    localparam IN_WIDTH         = MODULE_WIDTH * TOTAL_MODULES,
+    localparam TOTAL_DEPTH      = COL_X * TOTAL_INPUT_W,
+    localparam MEMORY_SIZE      = TOTAL_DEPTH * MODULE_WIDTH,
+    localparam int ADDR_WIDTH   = $clog2(TOTAL_DEPTH)
 ) (
     input logic clk, rst_n,
 
-    output logic 
+    // Write Interface (Stage X)
+    input logic                     wr_valid,
+    input logic [IN_WIDTH-1:0]      wr_data [TOTAL_INPUT_W],
+    output logic                    wr_ready, // Buffer can accept writes
+
+    // Read Interface (Stage Y)
+    input logic                     rd_ready,
+    output logic                    rd_valid,
+    output logic [MODULE_WIDTH-1:0] rd_data, 
+    output logic [ADDR_WIDTH-1:0]   rd_addr
+
+    // Debug
+    output logic                active_bank_wr,
+    output logic                active_bank_rd,
 );
+    // ************************************ Controller ************************************
+
+
+
+    // ************************************ Write BRAM ************************************
+    xpm_memory_tdpram
+    #(
+        // Common module parameters
+        .MEMORY_SIZE(MEMORY_SIZE),           // DECIMAL, 
+        .MEMORY_PRIMITIVE("auto"),           // String
+        .CLOCKING_MODE("common_clock"),      // String, "common_clock"
+        .MEMORY_INIT_FILE(),                 // String
+        .MEMORY_INIT_PARAM("0"),             // String      
+        .USE_MEM_INIT(1),                    // DECIMAL
+        
+        // Port A module parameters
+        .WRITE_DATA_WIDTH_A(MODULE_WIDTH), // DECIMAL, data width: 64-bit
+        .READ_DATA_WIDTH_A(MODULE_WIDTH),  // DECIMAL, data width: 64-bit
+        .BYTE_WRITE_WIDTH_A($clog2(MODULE_WIDTH)), // DECIMAL, how many bytes in WRITE_DATA_WIDTH_A, use $clog2 maybe?
+        .ADDR_WIDTH_A(ADDR_WIDTH),         // DECIMAL, clog2(MEMORY_SIZE/WRITE_DATA_WIDTH_A)
+        .READ_RESET_VALUE_A("0"),            // String
+        .READ_LATENCY_A(1),                  // DECIMAL
+        .WRITE_MODE_A("write_first"),        // String
+        .RST_MODE_A("SYNC"),                 // String
+        
+        // Port B module parameters  
+        .WRITE_DATA_WIDTH_B(MODULE_WIDTH), // DECIMAL, data width: 64-bit
+        .READ_DATA_WIDTH_B(MODULE_WIDTH),  // DECIMAL, data width: 64-bit
+        .BYTE_WRITE_WIDTH_B($clog2(MODULE_WIDTH)), // DECIMAL
+        .ADDR_WIDTH_BADDR_WIDTH(),         // DECIMAL, clog2(MEMORY_SIZE/WRITE_DATA_WIDTH_A)
+        .READ_RESET_VALUE_B("0"),            // String
+        .READ_LATENCY_B(1),                  // DECIMAL
+        .WRITE_MODE_B("write_first"),        // String
+        .RST_MODE_B("SYNC")                  // String
+    )
+    bank_0
+    (
+        // Port A module ports
+        .clka(clk),
+        .rsta(~rst_n),
+        .ena(1'b0), 
+        .wea(),
+        .addra(), 
+        .dina(),
+        .douta(),
+        
+        // Port B module ports
+        .clkb(clk),
+        .rstb(~rst_n),
+        .enb(),
+        .web('0), 
+        .addrb(),
+        .dinb(),
+        .doutb() // For now, we only use port B to read
+    );
+
+    // ************************************ Read BRAM ************************************
+    xpm_memory_tdpram
+    #(
+        // Common module parameters
+        .MEMORY_SIZE(MEMORY_SIZE),           // DECIMAL, 
+        .MEMORY_PRIMITIVE("auto"),           // String
+        .CLOCKING_MODE("common_clock"),      // String, "common_clock"
+        .MEMORY_INIT_FILE(),                 // String
+        .MEMORY_INIT_PARAM("0"),             // String      
+        .USE_MEM_INIT(1),                    // DECIMAL
+        
+        // Port A module parameters
+        .WRITE_DATA_WIDTH_A(MODULE_WIDTH), // DECIMAL, data width: 64-bit
+        .READ_DATA_WIDTH_A(MODULE_WIDTH),  // DECIMAL, data width: 64-bit
+        .BYTE_WRITE_WIDTH_A($clog2(MODULE_WIDTH)), // DECIMAL, how many bytes in WRITE_DATA_WIDTH_A, use $clog2 maybe?
+        .ADDR_WIDTH_A(ADDR_WIDTH),         // DECIMAL, clog2(MEMORY_SIZE/WRITE_DATA_WIDTH_A)
+        .READ_RESET_VALUE_A("0"),            // String
+        .READ_LATENCY_A(1),                  // DECIMAL
+        .WRITE_MODE_A("write_first"),        // String
+        .RST_MODE_A("SYNC"),                 // String
+        
+        // Port B module parameters  
+        .WRITE_DATA_WIDTH_B(MODULE_WIDTH), // DECIMAL, data width: 64-bit
+        .READ_DATA_WIDTH_B(MODULE_WIDTH),  // DECIMAL, data width: 64-bit
+        .BYTE_WRITE_WIDTH_B($clog2(MODULE_WIDTH)), // DECIMAL
+        .ADDR_WIDTH_BADDR_WIDTH(),         // DECIMAL, clog2(MEMORY_SIZE/WRITE_DATA_WIDTH_A)
+        .READ_RESET_VALUE_B("0"),            // String
+        .READ_LATENCY_B(1),                  // DECIMAL
+        .WRITE_MODE_B("write_first"),        // String
+        .RST_MODE_B("SYNC")                  // String
+    )
+    xpm_memory_tdpram_inst
+    (
+        // Port A module ports
+        .clka(clk),
+        .rsta(~rst_n),
+        .ena(1'b0), 
+        .wea(),
+        .addra(), 
+        .dina(),
+        .douta(),
+        
+        // Port B module ports
+        .clkb(clk),
+        .rstb(~rst_n),
+        .enb(),
+        .web('0), 
+        .addrb(),
+        .dinb(),
+        .doutb() // For now, we only use port B to read
+    );
+
+
+
 endmodule
