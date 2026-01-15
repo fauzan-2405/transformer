4c6dac09677ae12c90d91c48ae0ed6171ff37e6d
fauzan-2405 <fauzaanmuhammaad@gmail.com>
2026-01-15 10:47:21 +0700

    minor update: added local parameters for North and West Buffer configuration in buffer0_pkg

--- Multi-Head Attention/Bridge Buffer/buffer0_pkg.sv
+++ Multi-Head Attention/Bridge Buffer/buffer0_pkg.sv
@@ -13,14 +13,26 @@ package buffer0_pkg;
     parameter int W0_NUM_CORES_A    = self_attention_pkg::NUM_CORES_A_Qn_KnT;
     parameter int W0_NUM_CORES_B    = 1;
     parameter int W0_TOTAL_MODULES  = self_attention_pkg::TOTAL_MODULES_LP_Q; // How many modules used from the last multiplication for this west buffer
+    localparam W0_SLICE_WIDTH       = WIDTH*(top_pkg::TOP_CHUNK_SIZE)*W0_NUM_CORES_A;
+    localparam W0_MODULE_WIDTH      = W0_SLICE_WIDTH*TOTAL_INPUT_W_W0;
+    localparam W0_IN_WIDTH          = W0_SLICE_WIDTH * W0_NUM_CORES_B * W0_TOTAL_MODULES;
+    localparam W0_TOTAL_DEPTH       = W0_ROW_X * W0_COL_X; // Can be reduced even further
+    localparam W0_MEMORY_SIZE       = W0_TOTAL_DEPTH * W0_MODULE_WIDTH;
+    localparam int ADDR_WIDTH_W0    = $clog2(W0_TOTAL_DEPTH);
 
-    // For North Ping-Pong Buffer, PLEASE CHANGE THESE PARAMETERS ACCORDING TO YOUR USAGE
+    // For North Buffer, PLEASE CHANGE THESE PARAMETERS ACCORDING TO YOUR USAGE
     parameter TOTAL_INPUT_W_N0      = 2;
     parameter int N0_ROW_X          = W_COL_X;   
     parameter int N0_COL_X          = W_ROW_X
     parameter int N0_NUM_CORES_A    = 1;
     parameter int N0_NUM_CORES_B    = self_attention_pkg::NUM_CORES_B_Qn_KnT;
     parameter int N0_TOTAL_MODULES  = self_attention_pkg::TOTAL_MODULES_LP_K; // How many modules used from the last multiplication for this west buffer
+    localparam N0_SLICE_WIDTH       = WIDTH*(top_pkg::TOP_CHUNK_SIZE)*N0_NUM_CORES_B;
+    localparam N0_MODULE_WIDTH      = N0_SLICE_WIDTH*TOTAL_INPUT_W_N0;
+    localparam N0_IN_WIDTH          = N0_SLICE_WIDTH * N0_NUM_CORES_A * N0_TOTAL_MODULES;
+    localparam N0_TOTAL_DEPTH       = N0_ROW_X * N0_COL_X;
+    localparam N0_MEMORY_SIZE       = N0_TOTAL_DEPTH * N0_MODULE_WIDTH;
+    localparam int ADDR_WIDTH_N0    = $clog2(N0_TOTAL_DEPTH);
 
     parameter int ROW_SIZE_MAT_C_B0 = W0_ROW_X / TOTAL_INPUT_W_W0; 
     parameter int COL_SIZE_MAT_C_B0 = N0_COL_X / TOTAL_INPUT_W_N0;
