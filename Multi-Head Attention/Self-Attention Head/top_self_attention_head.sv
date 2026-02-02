f1dc73fed1deb5e728e2166acad8ad57af484236
fauzan-2405 <fauzaanmuhammaad@gmail.com>
2026-02-02 18:33:07 +0700

    initial commit

---
+++ Multi-Head Attention/Self-Attention Head/top_self_attention_head.sv
@@ -0,0 +1,12 @@
+// top_self_attention_head.sv
+// This is the top module for self_attention_head + self_attention_ctrl
+
+module top_self_attention_head #(
+    parameter XXX = YYY
+) (
+    input logic clk, rst_n,
+
+    output logic xxx
+);
+    
+endmodule
