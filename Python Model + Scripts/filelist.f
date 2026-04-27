d907919617841db333a3fd9b351c950077940626
fauzan-2405 <fauzaanmuhammaad@gmail.com>
2026-04-27 14:42:20 +0700

    feat: reorganize file paths in filelist.f for better structure and clarity

--- Python Model + Scripts/filelist.f
+++ Python Model + Scripts/filelist.f
@@ -4,7 +4,7 @@
 /mnt/ssd/mfauzan/transformer/source_code_real/top_pkg.sv
 /mnt/ssd/mfauzan/transformer/source_code_real/linear_proj_pkg.sv
 /mnt/ssd/mfauzan/transformer/source_code_real/self_attention_pkg.sv
-/mnt/ssd/mfauzan/transformer/source_code_real/buffer0_pkg.sv
+/mnt/ssd/mfauzan/transformer/source_code_real/buffer/buffer0_pkg.sv
 
 # =========================
 # BASIC BUILDING BLOCKS
@@ -14,7 +14,7 @@
 /mnt/ssd/mfauzan/transformer/source_code_real/pe_v2.v
 /mnt/ssd/mfauzan/transformer/source_code_real/mux2_1.v
 /mnt/ssd/mfauzan/transformer/source_code_real/control_mux2_1.v
-/mnt/ssd/mfauzan/transformer/source_code_real/ram_1w2r.v
+/mnt/ssd/mfauzan/transformer/source_code_real/softmax/ram_1w2r.v
 /mnt/ssd/mfauzan/transformer/source_code_real/ram_1w_1r.v
 
 # =========================
@@ -32,23 +32,23 @@
 # =========================
 # SOFTMAX + SUPPORTING MODULES
 # =========================
-/mnt/ssd/mfauzan/transformer/source_code_real/amult.v
-/mnt/ssd/mfauzan/transformer/source_code_real/exp_vec.v
-/mnt/ssd/mfauzan/transformer/source_code_real/lnu.v
-/mnt/ssd/mfauzan/transformer/source_code_real/lnu_range_adapter_1to8.v
-/mnt/ssd/mfauzan/transformer/source_code_real/softmax_vec.v
+/mnt/ssd/mfauzan/transformer/source_code_real/softmax/amult.v
+/mnt/ssd/mfauzan/transformer/source_code_real/softmax/exp_vec.v
+/mnt/ssd/mfauzan/transformer/source_code_real/softmax/lnu.v
+/mnt/ssd/mfauzan/transformer/source_code_real/softmax/lnu_range_adapter_1to8.v
+/mnt/ssd/mfauzan/transformer/source_code_real/softmax/softmax_vec.v
 /mnt/ssd/mfauzan/transformer/source_code_real/rshift.sv
 
 # =========================
 # BUFFER SYSTEM
 # =========================
-/mnt/ssd/mfauzan/transformer/source_code_real/buffer_ctrl.sv
-/mnt/ssd/mfauzan/transformer/source_code_real/buffer_ctrl_special.sv
-/mnt/ssd/mfauzan/transformer/source_code_real/buffer_n.sv
-/mnt/ssd/mfauzan/transformer/source_code_real/buffer_n_special.sv
-/mnt/ssd/mfauzan/transformer/source_code_real/buffer_w.sv
-/mnt/ssd/mfauzan/transformer/source_code_real/buffer_wrapper.sv
-/mnt/ssd/mfauzan/transformer/source_code_real/top_buffer.sv
+/mnt/ssd/mfauzan/transformer/source_code_real/buffer/buffer_ctrl.sv
+/mnt/ssd/mfauzan/transformer/source_code_real/buffer/buffer_ctrl_special.sv
+/mnt/ssd/mfauzan/transformer/source_code_real/buffer/buffer_n.sv
+/mnt/ssd/mfauzan/transformer/source_code_real/buffer/buffer_n_special.sv
+/mnt/ssd/mfauzan/transformer/source_code_real/buffer/buffer_w.sv
+/mnt/ssd/mfauzan/transformer/source_code_real/buffer/buffer_wrapper.sv
+/mnt/ssd/mfauzan/transformer/source_code_real/buffer/top_buffer.sv
 
 # =========================
 # DATAFLOW / CONVERTERS
@@ -68,8 +68,8 @@
 # =========================
 # SELF ATTENTION
 # =========================
-/mnt/ssd/mfauzan/transformer/source_code_real/self_attention_ctrl.sv
-/mnt/ssd/mfauzan/transformer/source_code_real/self_attention_head.sv
-/mnt/ssd/mfauzan/transformer/source_code_real/top_self_attention_head.sv
-/mnt/ssd/mfauzan/transformer/source_code_real/multihead_attention.sv
-/mnt/ssd/mfauzan/transformer/source_code_real/tb_multihead_attention_script.sv
+/mnt/ssd/mfauzan/transformer/source_code_real/self_attention_head/self_attention_ctrl.sv
+/mnt/ssd/mfauzan/transformer/source_code_real/self_attention_head/self_attention_head.sv
+/mnt/ssd/mfauzan/transformer/source_code_real/self_attention_head/top_self_attention_head.sv
+/mnt/ssd/mfauzan/transformer/source_code_real/multi_head_attention/multihead_attention.sv
+/mnt/ssd/mfauzan/transformer/source_code_real/multi_head_attention/tb_multihead_attention_script.sv
