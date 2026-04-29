06dc0839085064d2fe585178ac96fae50c17977a
fauzan-2405 <fauzaanmuhammaad@gmail.com>
2026-04-29 13:05:03 +0700

    feat: update run_sim_tb.tcl to enhance testbench elaboration with memory file parameters

--- Python Model + Scripts/run_sim_tb.tcl
+++ Python Model + Scripts/run_sim_tb.tcl
@@ -14,8 +14,11 @@ exec xvlog -sv -f /mnt/ssd/mfauzan/transformer/python_code/filelist.f
 # Elaborate testbench
 puts "Elaborating..."
 
-#exec xelab /mnt/ssd/mfauzan/transformer/source_code/multi_head_attention/tb_multihead_attention_script -s tb_sim --relax --debug typical -L xpm
-exec xelab tb_multihead_attention_script -s tb_sim
+exec xelab tb_multihead_attention -s tb_sim \
+    --relax --debug typical -L xpm \
+    -generic_top {MEM_INIT_Q_FILE=/mnt/ssd/mfauzan/transformer/source_code_real/mat_B_lp_bridge.mem} \
+    -generic_top {MEM_INIT_K_FILE=/mnt/ssd/mfauzan/transformer/source_code_real/mat_B_lp_bridge.mem} \
+    -generic_top {MEM_INIT_V_FILE=/mnt/ssd/mfauzan/transformer/source_code_real/mat_B_lp_bridge.mem}
 
 # Run simulation with plusargs
 puts "Running simulation..."
@@ -24,3 +27,5 @@ exec xsim tb_sim \
     -runall \
     -testplusarg OUT_DIR=/mnt/ssd/mfauzan/transformer/python_code/exports_1/hardware \
     -testplusarg INPUT_FILE=/mnt/ssd/mfauzan/transformer/source_code/linproj_mem/mem_A_lp_bridge.mem
+
+puts "TCL simulation all done"
