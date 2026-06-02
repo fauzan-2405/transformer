9bb570b3f2c797eb0adafa3dd04979a6bdf5a3fa
fauzan-2405 <fauzaanmuhammaad@gmail.com>
2026-06-02 13:39:18 +0700

    Refactor run_sim.tcl to enhance width precision configuration for input, weight, softmax, and final matrices; update argument parsing for improved clarity and functionality.

--- Python Model + Scripts/run_sim.tcl
+++ Python Model + Scripts/run_sim.tcl
@@ -22,13 +22,25 @@ set mem_q       [lindex $argv 2]
 set mem_k       [lindex $argv 3]
 set mem_v       [lindex $argv 4]
 
-set width       [lindex $argv 5]
-set frac        [lindex $argv 6]
-set i_mat       [lindex $argv 7]
-set inner_mat   [lindex $argv 8]
-set w_mat       [lindex $argv 9]
-set cores_a     [lindex $argv 10]
-set tot_modules [lindex $argv 11]
+# Width precision
+set in_width    [lindex $argv 5]
+set in_frac     [lindex $argv 6]
+set w_width     [lindex $argv 7]
+set w_frac      [lindex $argv 8]
+#set key_width   [lindex $argv 9]
+#set key_frac    [lindex $argv 10]
+#set qkt_width   [lindex $argv 11]
+#set qkt_frac    [lindex $argv 12]
+set soft_width  [lindex $argv 9]
+set soft_frac   [lindex $argv 10]
+set final_width [lindex $argv 11]
+set final_frac  [lindex $argv 12]
+
+set i_mat       [lindex $argv 13]
+set inner_mat   [lindex $argv 14]
+set w_mat       [lindex $argv 15]
+set cores_a     [lindex $argv 16]
+set tot_modules [lindex $argv 17]
 
 # Clean
 file delete -force xsim.dir
@@ -36,8 +48,18 @@ file delete -force *.log *.jou *.pb
 
 puts "Compiling..."
 exec xvlog -sv \
-    -d SYSTEM_TOP_WIDTH=$width \
-    -d SYSTEM_FRAC_WIDTH=$frac \
+    -d SYSTEM_TOP_WIDTH_INPUT=$in_width \
+    -d SYSTEM_FRAC_WIDTH_INPUT=$in_frac \
+
+    -d SYSTEM_TOP_WIDTH_WEIGHT=$w_width \
+    -d SYSTEM_FRAC_WIDTH_WEIGHT=$w_frac \
+
+    -d SYSTEM_TOP_WIDTH_SOFTMAX=$soft_width \
+    -d SYSTEM_FRAC_WIDTH_SOFTMAX=$soft_frac \
+
+    -d SYSTEM_TOP_WIDTH_FINAL=$final_width \
+    -d SYSTEM_FRAC_WIDTH_FINAL=$final_frac \
+
     -d I_MATRIX_DIMENSION=$i_mat \
     -d INNER_MATRIX_DIMENSION=$inner_mat \
     -d W_MATRIX_DIMENSION=$w_mat \
