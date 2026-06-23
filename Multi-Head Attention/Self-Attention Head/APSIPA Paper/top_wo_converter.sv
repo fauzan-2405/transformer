aecbe35fa90730abdcc3aeae37b473f54c35242b
fauzan-2405 <fauzaanmuhammaad@gmail.com>
2026-06-23 15:15:13 +0700

    Updated with the fifo and the controller

--- Multi-Head Attention/Self-Attention Head/APSIPA Paper/top_wo_converter.sv
+++ Multi-Head Attention/Self-Attention Head/APSIPA Paper/top_wo_converter.sv
@@ -9,8 +9,11 @@ module top_wo_converter #(
 
     // Elements Parameters 
     parameter WIDTH_A       = 16,
+    parameter FRAC_WIDTH_A  = 8,
     parameter WIDTH_B       = 16,
+    parameter FRAC_WIDTH_B  = 8,
     parameter WIDTH_OUT     = 16,
+    parameter FRAC_WIDTH_OUT= 8,
     parameter BLOCK_SIZE    = 2,
     parameter CHUNK_SIZE    = BLOCK_SIZE * BLOCK_SIZE,
     parameter NUM_CORES_A   = 2,
@@ -34,7 +37,6 @@ module top_wo_converter #(
     input wire                  s_axis_1_tlast,
 
     // *** Custom IP port (optional) ***
-    output wire                 computation_done,
     
     // *** AXIS master port ***
     input wire                  m_axis_tready,
@@ -42,6 +44,12 @@ module top_wo_converter #(
     output wire                 m_axis_tvalid,
     output wire                 m_axis_tlast
 );
+    localparam int NUM_A_ELEMENTS = ((A_OUTER_DIMENSION/BLOCK_SIZE)*(INNER_DIMENSION/BLOCK_SIZE))/(NUM_CORES_A); // Total elements of Input if we converted the inputs based on the NUM_CORES, in tb_multihead_attention.sv
+    localparam int NUM_B_ELEMENTS = ((B_OUTER_DIMENSION/BLOCK_SIZE)*(INNER_DIMENSION/BLOCK_SIZE))/(NUM_CORES_B);
+
+    localparam int ROW_SIZE_MAT_C = A_OUTER_DIMENSION / (BLOCK_SIZE * NUM_CORES_A);  
+    localparam int COL_SIZE_MAT_C = B_OUTER_DIMENSION / (BLOCK_SIZE * NUM_CORES_B); 
+    localparam int MAX_FLAG = (ROW_SIZE_MAT_C * COL_SIZE_MAT_C);
 
     // ========================================= INPUT BRAM =========================================
     parameter MEMORY_SIZE_A = INNER_DIMENSION*A_OUTER_DIMENSION*WIDTH_A;
@@ -49,7 +57,7 @@ module top_wo_converter #(
     parameter int ADDR_WIDTH_A = $clog2(MEMORY_SIZE_A/DATA_WIDTH_A);
 
     logic in_mat_ena, in_mat_enb;
-    logic in_mat_wea, in_mat_web;
+    logic in_mat_wea;
     logic [ADDR_WIDTH_A-1:0] in_mat_addra, in_mat_addrb;
     logic [DATA_WIDTH_A-1:0] in_mat_doutb;
     
@@ -92,21 +100,21 @@ module top_wo_converter #(
     xpm_memory_tdpram_in_mat
     (        
         // Port A module ports
-        .clka(clk),
-        .rsta(~rst_n),
+        .clka(aclk),
+        .rsta(~aresetn),
         .ena(in_mat_ena),
         .wea(in_mat_wea),
         .addra(in_mat_addra), 
         .dina(s_axis_0_tdata),
-        .douta(),
+        .douta(1'b0),
         
         // Port B module ports
-        .clkb(clk),
-        .rstb(~rst_n),
+        .clkb(aclk),
+        .rstb(~aresetn),
         .enb(in_mat_enb),
-        .web(), 
+        .web(1'b0), 
         .addrb(in_mat_addrb), 
-        .dinb(),
+        .dinb(1'b0),
         .doutb(in_mat_doutb)
     );
 
@@ -117,7 +125,7 @@ module top_wo_converter #(
     parameter int ADDR_WIDTH_B = $clog2(MEMORY_SIZE_B/DATA_WIDTH_B);
 
     logic we_mat_ena, we_mat_enb;
-    logic we_mat_wea, we_mat_web;
+    logic we_mat_wea;
     logic [ADDR_WIDTH_A-1:0] we_mat_addra, we_mat_addrb;
     logic [DATA_WIDTH_A-1:0] we_mat_doutb;
 
@@ -160,39 +168,68 @@ module top_wo_converter #(
     xpm_memory_tdpram_we_mat
     (        
         // Port A module ports
-        .clka(clk),
-        .rsta(~rst_n),
+        .clka(aclk),
+        .rsta(~aresetn),
         .ena(we_mat_ena),
         .wea(we_mat_wea),
         .addra(we_mat_addra), 
         .dina(s_axis_1_tdata),
-        .douta(),
+        .douta(1'b0),
         
         // Port B module ports
-        .clkb(clk),
-        .rstb(~rst_n),
+        .clkb(aclk),
+        .rstb(~aresetn),
         .enb(we_mat_enb),
-        .web(), 
+        .web(1'b0), 
         .addrb(we_mat_addrb), 
-        .dinb(),
+        .dinb(1'b0),
         .doutb(we_mat_doutb)
     );
+
+    // ========================================= MATMUL MODULE =========================================
+    logic acc_done;
+    logic sys_finish;
+    logic [WIDTH_OUT_C-1:0] out_matmul;
+
+    matmul_module #(
+        .INNER_DIMENSION(INNER_DIMENSION),
+        .CHUNK_SIZE(CHUNK_SIZE),
+        .WIDTH_A(WIDTH_A),
+        .FRAC_WIDTH_A(FRAC_WIDTH_A),
+        .WIDTH_B(WIDTH_B),
+        .FRAC_WIDTH_B(FRAC_WIDTH_B),
+        .WIDTH_OUT(WIDTH_OUT),
+        .FRAC_WIDTH_OUT(FRAC_WIDTH_OUT),
+        .NUM_CORES_A(NUM_CORES_A),
+        .NUM_CORES_B(NUM_CORES_B)
+    ) matmul_module_inst (
+        .clk(aclk), 
+        .en(en), 
+        .rst_n(internal_rst_n), 
+        .reset_acc(internal_reset_acc),
+        .input_w(in_mat_doutb), 
+        .input_n(we_mat_doutb), // The first one is the MSB
+        .accumulator_done(acc_done), 
+        .systolic_finish(sys_finish),
+        .out_top(out_matmul) // The first one is the MSB
+    );
+
 
     // ========================================= OUTPUT FIFO =========================================
     // xpm_fifo_axis: AXI Stream FIFO
     // Xilinx Parameterized Macro, version 2018.3
-    localparam FIFO_0_DEPTH                     = 16;  // Must be power of two
+    localparam FIFO_0_DEPTH                     = (MAX_FLAG < 16) ? 16 : MAX_FLAG;  // Must be power of two
                                                         /* 
                                                         Technically, it could be just 1, because 
                                                         the larger the matrix, the longer the cycle 
                                                         between the output
                                                         */
     localparam FIFO_0_WR_RD_DATA_COUNT_WIDTH    = $clog2(FIFO_0_DEPTH) + 1; 
-    localparam FIFO_0_TDATA_WIDTH               = OUT_MULTIHEAD; // Defines the width of the TDATA port, s_axis_tdata, and m_axis_tdata
+    localparam FIFO_0_TDATA_WIDTH               = WIDTH_OUT_C; // Defines the width of the TDATA port, s_axis_tdata, and m_axis_tdata
     localparam FIFO_0_TKEEP_WIDTH               = FIFO_0_TDATA_WIDTH / 8;
 
     logic s2mm_ready;
-    logic [OUT_MULTIHEAD-1:0] s2mm_data;
+    logic [WIDTH_OUT_C-1:0] s2mm_data
     logic s2mm_valid;
     logic s2mm_last;
 
@@ -245,70 +282,127 @@ module top_wo_converter #(
         .m_axis_tready(m_axis_tready), // ready  
         .m_axis_tdata(m_axis_tdata), // data
         .m_axis_tvalid(m_axis_tvalid), // valid
-        .m_axis_tdest(), 
-        .m_axis_tid(), 
-        .m_axis_tkeep(), 
+        .m_axis_tdest(1'b0), 
+        .m_axis_tid(1'b0), 
+        .m_axis_tkeep({FIFO_0_TKEEP_WIDTH{1'b1}}), 
         .m_axis_tlast(m_axis_tlast), 
-        .m_axis_tstrb(), 
-        .m_axis_tuser(),  
+        .m_axis_tstrb({FIFO_0_TKEEP_WIDTH{1'b1}}), 
+        .m_axis_tuser(1'b0),  
         
         .wr_data_count_axis() // data count
     );
 
     // ========================================= CONTROLLER =========================================
     // The controller goes here
-    logic idx_out;  // Because TOTAL_INPUT_W_Qn_KnT == 2
-    logic idx_out_reg;  // Delayed version
+    logic internal_reset_acc, internal_rst_n;
+    logic acc_done_rising;
+    logic acc_done_d;
+    assign acc_done_rising = ~acc_done_d & acc_done;
+    logic en_module; // Toggle to ALWAYS HIGH after both BRAMs are filled
+    logic [WIDTH_OUT-1:0] counter, counter_row, counter_col, flag;
+    logic counter_acc_done, matmul_done;
 
     // Combinational controller
     assign s_axis_0_tready  = in_mat_ena;
     assign s_axis_1_tready  = in_mat_enb;
     assign in_mat_wea       = s_axis_0_tvalid;
-    assign in_mat_web       = s_axis_1_tvalid;
-    assign in_mat_dina      = s_axis_0_tdata;
-    assign in_mat_dinb      = s_axis_1_tdata;
-    assign computation_done = QKT_Vn_done;
-    assign s2mm_valid       = (idx_out_reg || idx_out);
-    assign s2mm_last        = QKT_Vn_done && idx_out_reg;
+    assign we_mat_wea       = s_axis_1_tvalid;
+    assign s2mm_data        = out_matmul;
+    assign s2mm_valid       = (counter_acc_done);
+    assign s2mm_last        = counter_acc_done && matmul_done;
 
     // Sequential controller
     always_ff @(posedge aclk) begin
         if (~aresetn) begin
-            idx_out         <= 0;
-            idx_out_reg     <= 0;
             in_mat_ena      <= 0;
             in_mat_enb      <= 0;
-            in_mat_wr_addra <= '0;
-            in_mat_wr_addrb <= 1;
+            in_mat_addra    <= 0;
+            in_mat_addrb    <= 0;
+            we_mat_addra    <= 0;
+            we_mat_addrb    <= 0;
 
+            acc_done_d      <= 0;
+            internal_rst_n      <= 0;
+            internal_reset_acc  <= 0;
+            counter         <= 0;
+            counter_row     <= 0;
+            counter_col     <= 0;
+            counter_acc_done <= 0;
+            flag            <= 0;
+            matmul_done     <= 0;
         end else begin
-            // ================== Write input BRAM ==================
-            
-            if (~s_axis_0_tlast) in_mat_ena  <= 1;
-            if (~s_axis_1_tlast) in_mat_enb  <= 1;
-            if (in_mat_wea) begin
-                // Port A: even
-                in_mat_wr_addra     <= in_mat_wr_addra + 2;
+            // ================== Write Input & Weight BRAM ==================
+            if (m_axis_tlast) begin
+                in_mat_ena  <= 0;
+                in_mat_enb  <= 0;
+            end else begin
+                in_mat_ena  <= 1;
+                in_mat_enb  <= 1;
             end
+
+            if (in_mat_wea) begin
+                in_mat_addra    <= in_mat_addra + 1;
+            end
+
+            if (we_mat_wea) begin
+                we_mat_addra    <= we_mat_addra + 1;
+            end
+
+            // ================== Read Input & Weight BRAM ==================
+            acc_done_d  <= acc_done;
+            counter_acc_done <= 0;
             
-            if (in_mat_web) begin
-                // Port B: odd
-                in_mat_wr_addrb     <= in_mat_wr_addrb + 2;
+            // Port A & B Controller
+            if ((in_mat_addra >= NUM_A_ELEMENTS-1) && (we_mat_addra >= NUM_B_ELEMENTS-1)) begin // enable AFTER BOTH of BRAMs are filled is HIGH
+                en_module <= 1'b1;// Control write enable of each BRAMs for both ports
+            end
+
+            // Internal Reset Control
+            if (en_module) begin
+                internal_rst_n  <= ~sys_finish;
+            end
+
+            if (sys_finish) begin
+                internal_reset_acc <= ~acc_done;
+            end
+
+            // Counter Update
+            if (sys_finish) begin
+                // counter indicates the matrix C element iteration
+                if (counter == ((INNER_DIMENSION/BLOCK_SIZE) - 1)) begin 
+                    counter     <= 0;
+                end
+                else begin
+                    counter     <= counter + 1;
+                end
+                // Address controller
+                in_mat_addrb    <= counter + (INNER_DIMENSION/BLOCK_SIZE)*counter_row;
+                we_mat_addrb    <= counter + (INNER_DIMENSION/BLOCK_SIZE)*counter_col;
+            end
+
+            // Column/Row Update
+            if (acc_done_rising) begin
+                // counter_row indicates the i-th row of the matrix C that we are working right now
+                // counter_col indicates the i-th column of the matrix C that we are working right now
+
+                // Check if we already at the end of the MAT C column
+                if (counter_col == (COL_SIZE_MAT_C - 1)) begin
+                    counter_col <= 0;
+                    counter_row <= counter_row + 1;
+                end else begin
+                    counter_col <= counter_col + 1;
+                end
+
+                counter_acc_done <= 1;
+
+                // Flag assigning for 'done' variable
+                if (flag != MAX_FLAG) begin
+                    flag <= flag + 1;   
+                end else if (flag >= MAX_FLAG - 1)
+                    matmul_done <= 1;
             end
 
             // ================== Output FIFO takes the output fromt he multihead attention ==================   
-            idx_out_reg     <= idx_out;
-                     
-            if (out_QKT_Vn_valid) begin
-                if (~idx_out) begin
-                    idx_out <= 1;
-                end
-            end else begin
-                idx_out <= 0;
-            end
-            
-            
-            s2mm_data   <= out_matmul_QKT_Vn[0][idx_out];
         end
     end
 
